8ac98d4a8d5f139d5605269e08a96683d2b50ef6
AsakuraRET <emelyatop@gmail.com>
2020-01-22 11:49:19 +0200

    add database

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ gem 'tilt'          # Templating engine
 gem 'erubi'         # Template syntax
 gem 'puma'          # Web application server
 gem 'rack-unreloader'           # Allows code reloading
+gem "pg", "~> 0.18.1"
+gem "sequel"
--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'roda'
+require 'sequel'
 require 'tilt'
 
+database = "app_development"
+user     = ENV["asakura"]
+password = ENV["Scrum123"]
+DB = Sequel.connect(adapter: "postgres", database: database, host: "127.0.0.1", user: user, password: password)
 
 class App < Roda
   plugin :render, escape: true
